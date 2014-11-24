07328c12f0a2c30a6e0fa0d4c1e13e3370c3bffb
Yogesh <Yogesh@Sameer>
2014-11-24 20:07:28 +0530

    Question paper generator

---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+session_start();
+?>
+<html>
+<head>
+    <title>Sign up page</title> 
+ 
+</head>
+<body>
+<?php
+ include ('header.php');?>
+
+<div id="bottom">
+<hr>
+<h1><u>CONTACT US HERE!</u></h1>
+<hr>
+<h2><b>For all queries please contact us at the below addresses:</b></h2>
+<h4>
+RIGHT CLICK SOLUTIONS.
+Head office : 403,Arun soc ,Near Om Super Market<br><br>
+Model colony,Pune.<br><br>
+Phone : +91 762 033 6672.<br><br>
+Timimngs : IST 10AM to 4 PM(Monday to Saturday) <br><br>
+Email :rightclicksolutions@gmail.com</h4>
+</div>
+</body>
+</html>
